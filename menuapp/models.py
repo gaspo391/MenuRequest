c064dd79fd84d1c42d02ac83ce65118bfaea3133
gaspo391 <gaspo391@gmail.com>
2020-05-23 21:28:01 +0900

    navbarのプルダウンメニューをメニュー、グループ、アカウント管理の3つに統合

--- menuapp/models.py
+++ menuapp/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+class Category(models.Model):
+  category = models.CharField(max_length=100, null=True, blank=True)
+
+  class Meta:
+    db_table = 'category'
+
+  def __str__(self):
+    return self.category
+
 # Create your models here.
 class Menu(models.Model):
   menuName = models.CharField(max_length=100, null=True, blank=True)
-  category = models.CharField(max_length=100, null=True, blank=True)
+  category = models.ForeignKey(Category, on_delete=models.PROTECT)
   image = models.ImageField(upload_to='media')
   items = models.TextField()
   author = models.CharField(max_length=200, null=True, blank=True)
