925f5922cd9b78669097e5333fd98dea165a20e5
gaspo391 <gaspo391@gmail.com>
2020-05-23 22:05:54 +0900

    メニュー登録バグ修正、カテゴリーをCharField

--- menuapp/models.py
+++ menuapp/models.py
@@ -12,7 +12,8 @@ class Category(models.Model):
 # Create your models here.
 class Menu(models.Model):
   menuName = models.CharField(max_length=100, null=True, blank=True)
-  category = models.ForeignKey(Category, on_delete=models.PROTECT)
+  # category = models.ForeignKey(Category, on_delete=models.PROTECT)
+  category = models.CharField(max_length=100, null=True, blank=True)
   image = models.ImageField(upload_to='media')
   items = models.TextField()
   author = models.CharField(max_length=200, null=True, blank=True)
